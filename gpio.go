b385c36871202b8b89815c00ec1e3ee594a21872
Frank Burnham <fra.burnham@gmail.com>
2016-08-05 21:35:24 -0400

    Add Interrupt to GPIO

--- gpio.go
+++ gpio.go
@@ -1,9 +1,17 @@
 package gpio
 
+import "time"
+
+type InterruptEvent struct {
+	value int
+	timestamp time.Time
+}
+
 type GPIO interface {
 	MakeOutput() error
 	MakeInput() error
 	WriteValue(int) error
 	ReadValue() (int, error)
+	Interrupt(string, chan InterruptEvent) error
 	Close() error
 }
